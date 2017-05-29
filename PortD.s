d7fe3ddd10f4f4da982a7946d738d128e6e6abdb
david.pinzonv <david.pinzonv@gmail.com>
2017-05-28 21:25:23 -0500

    Full implemented keypad 4x4, store key in 1 byte memory

--- PortD.s
+++ PortD.s
@@ -43,16 +43,29 @@ SYSCTL_PRGPIO_R				EQU 0x400FEA08	;General-Purpose Input/Output Peripheral Ready
 SYSCTL_RCGCGPIO_R3			EQU 0x00000008	;GPIO Port D Run Mode Clock Gating Control
 SYSCTL_PRGPIO_R3			EQU 0x00000008	;GPIO Port D Peripheral Ready
 	
+NVIC_EN0_R					EQU 0xE000E100  ; Interrupt 0 to 31 Set Enable Register
+NVIC_EN0_INT3				EQU 0x00000008  ; Interrupt 3 enable (GPIO PORT D)
+	
 
+		AREA    DATA, ALIGN=2
+KeyInput	SPACE	1
 
+		AREA    |.text|, CODE, READONLY, ALIGN=2
+		THUMB
+		EXPORT	KeyInput
 	;functions to export
 		EXPORT	InitPortD
 		EXPORT	PORTD_Handler
+		EXPORT	PortD_clearInterrupt
+		EXPORT	PortD_detectInterrupt
 			
-		AREA    |.text|, CODE, READONLY, ALIGN=2
-		THUMB
-			
+;---------InitPortD---------
+; Rutina de inicializacion del puerto D
+; Input : none
+; Output: none
+; Modifies: none, all used Register are pushed and poped
 InitPortD
+	PUSH {R0,R1,LR}					; save current values of R0,R1,LR
 	; activate clock for Port D
     LDR R1, =SYSCTL_RCGCGPIO_R      ; R1 = SYSCTL_RCGCGPIO_R (pointer)
     LDR R0, [R1]                    ; R0 = [R1] (value)
@@ -114,51 +127,185 @@ GPIOJinitloop
 	LDR R0,[R1]						; R0 = [R1] (value)
 	ORR R0, R0, #0x0F				; R0 = R0&~0x0F (interrupt on D0-D3 sent to the controller)
 	STR R0,[R1]						; [R1] = R0
-
+	
+	; enable interrupt 3 in NVIC
+    LDR R1, =NVIC_EN0_R             ; R1 = &NVIC_EN0_R (pointer)
+    LDR R0, =NVIC_EN0_INT3          ; R0 = NVIC_EN0_INT5 (zeros written to enable register have no effect)
+    STR R0, [R1]                    ; [R1] = R0
+	
+	; clear D4-D7
+	LDR R1,=GPIO_PORTD_4567			; R1 = GPIO_PORTD_4567 (pointer)
+	MOV R0,#0x00					; R0 = 0x00
+	STR R0,[R1]						; [R1] = R0 (clear D4-D7)
+	
+	POP {R0,R1,PC}					; restore previuos values
 
 ;---------PORTD_Handler---------
 ; Rutina de interrupcion para detectar tecla presionada
 ; Input : none
-; Output: none
+; Output: none (character stored in KeyInput)
 ; Modifies: none, all used Register are pushed and poped
 PORTD_Handler
-	PUSH {R0,R1,LR}					; save current values of R0,R1,LR
+	PUSH {R0,R1,R2,LR}					; save current values of R0,R1,LR
 	LDR R1,=GPIO_PORTD_RIS			; R1 = &GPIO_PORTD_RIS (pointer)
 	LDR R0, [R1]					; R0 = [R1] (GPIO_PORTD_RIS)	
 	;check raw interrupt to find the bit interrupt
 	CMP R0, #0x01					; R0 == 0x01 ? (D0 interrupt)
-	BNE	handler_check_D1
+	BNE	handler_check_D1			; if not, go to handler_check_D1
 	; acknowledge D0 interrupt
-	
+	BL	detect_line					; R0 = (line detected)
+	LDR R1,=KeyInput				; R1 = &KeyInput (pointer)
+	MOV R2, #'6'					; R2 = '6'
+	CMP R0,#4						; R0 == 4 ?(KEY D0,D4?)
+	STRBEQ R2,[R1]					; if so, [R1] = R2
+	MOV R2, #'3'					; R2 = '3'
+	CMP R0,#5						; R0 == 5 ?(KEY D0,D5?)
+	STRBEQ R2,[R1]					; if so, [R1] = R2
+	MOV R2, #'#'					; R2 = '#'
+	CMP R0,#6						; R0 == 6 ?(KEY D0,D6?)
+	STRBEQ R2,[R1]					; if so, [R1] = R2
+	MOV R2, #'9'					; R2 = '9'
+	CMP R0,#7						; R0 == 7 ?(KEY D0,D7?)
+	STRBEQ R2,[R1]					; if so, [R1] = R2
 handler_check_D1
 	CMP R0, #0x02					; R0 == 0x02 ? (D1 interrupt)
-	BNE	handler_check_D2
+	BNE	handler_check_D2			; if not, go to handler_check_D2
 	; acknowledge D1 interrupt
+	BL	detect_line					; R0 = (line detected)
+	LDR R1,=KeyInput				; R1 = &KeyInput (pointer)
+	MOV R2, #'5'					; R2 = '5'
+	CMP R0,#4						; R0 == 4 ?(KEY D1,D4?)
+	STRBEQ R2,[R1]					; if so, [R1] = R2
+	MOV R2, #'2'					; R2 = '2'
+	CMP R0,#5						; R0 == 5 ?(KEY D1,D5?)
+	STRBEQ R2,[R1]					; if so, [R1] = R2
+	MOV R2, #'0'					; R2 = '0'
+	CMP R0,#6						; R0 == 6 ?(KEY D1,D6?)
+	STRBEQ R2,[R1]					; if so, [R1] = R2
+	MOV R2, #'8'					; R2 = '8'
+	CMP R0,#7						; R0 == 7 ?(KEY D1,D7?)
+	STRBEQ R2,[R1]					; if so, [R1] = R2	
 handler_check_D2
 	CMP R0, #0x04					; R0 == 0x04 ? (D2 interrupt)
-	BNE	handler_check_D3
+	BNE	handler_check_D3			; if not, go to handler_check_D3
 	; acknowledge D2 interrupt
+	BL	detect_line					; R0 = (line detected)
+	LDR R1,=KeyInput				; R1 = &KeyInput (pointer)
+	MOV R2, #'B'					; R2 = 'B'
+	CMP R0,#4						; R0 == 4 ?(KEY D2,D4?)
+	STRBEQ R2,[R1]					; if so, [R1] = R2
+	MOV R2, #'A'					; R2 = 'A'
+	CMP R0,#5						; R0 == 5 ?(KEY D2,D5?)
+	STRBEQ R2,[R1]					; if so, [R1] = R2
+	MOV R2, #'D'					; R2 = 'D'
+	CMP R0,#6						; R0 == 6 ?(KEY D2,D6?)
+	STRBEQ R2,[R1]					; if so, [R1] = R2
+	MOV R2, #'C'					; R2 = 'C'
+	CMP R0,#7						; R0 == 7 ?(KEY D2,D7?)
+	STRBEQ R2,[R1]					; if so, [R1] = R2
 handler_check_D3
 	CMP R0, #0x08					; R0 == 0x08 ? (D3 interrupt)
-	BNE	handler_portd_done
+	BNE	handler_portd_done			; if not, go to handler_portd_done
 	; acknowledge D3 interrupt
+	BL	detect_line					; R0 = (line detected)
+	LDR R1,=KeyInput				; R1 = &KeyInput (pointer)
+	MOV R2, #'4'					; R2 = '4'
+	CMP R0,#4						; R0 == 4 ?(KEY D3,D4?)
+	STRBEQ R2,[R1]					; if so, [R1] = R2
+	MOV R2, #'1'					; R2 = '1'
+	CMP R0,#5						; R0 == 5 ?(KEY D3,D5?)
+	STRBEQ R2,[R1]					; if so, [R1] = R2
+	MOV R2, #'*'					; R2 = '*'
+	CMP R0,#6						; R0 == 6 ?(KEY D3,D6?)
+	STRBEQ R2,[R1]					; if so, [R1] = R2
+	MOV R2, #'7'					; R2 = '7'
+	CMP R0,#7						; R0 == 7 ?(KEY D3,D7?)
+	STRBEQ R2,[R1]					; if so, [R1] = R2	
 handler_portd_done
-	; clear interrupt
+	;No se limpian las interrupociones
+	;pograma VMC_MDB_MAIN debe limpiarlas
+	
+	POP {R0,R1,R2,PC}					; restore previuos values
+
+;---------PortD_clearInterrupt---------
+; Clear all raw interrupt vector of PORTD by writting FF in ICR
+; Input : none
+; Output: none
+; Modifies: all used Register are pushed and poped
+PortD_clearInterrupt
+	PUSH {R0,R1,LR}			; save current values of R0,R1,LR
 	LDR R1,=GPIO_PORTD_ICR	; R1 = GPIO_PORTD_ICR (pointer)
 	LDR R0,[R1]				; R0 = [R1] (value)
-	ORR R0,R0,#0xFF			; R0 = R0|0x01
+	MOV R0,#0xFF			; R0 = 0xFF
 	STR R0,[R1]				; [R1] = R0
+	POP {R0,R1,PC}			; restore previuos values
 	
-	POP {R0,R1,PC}					; restore previuos values
+;---------PortD_detectInterrupt---------
+; Detect if an interrupt on PortD has ocurred
+; Input : none
+; Output: R0 = 1 on interrupt detected, 0 if not
+; Modifies: R0 out, all used Register are pushed and poped
+PortD_detectInterrupt
+	PUSH {R1,LR}			; save current values of R0,R1,LR
+	LDR R1,=GPIO_PORTD_RIS	; R1 = GPIO_PORTD_RIS (pointer)
+	LDR R0,[R1]				; R0 = [R1] (value)
+	ANDS R0, R0,#0xFF		; R0 = R0|0xFF
+	POPEQ {R1,PC}			; If cero, no interrup, restore previuos values and return
+	MOV R0,#1				; interrupt ocurred
+	POP {R1,PC}				; restore previuos values
 	
 ;---------detect_line---------
 ; Encuentra la line de la tecla presionada
-; Input : R0, actual GPIO Raw Interrup Status
-; Output: none
-; Modifies: none, all used Register are pushed and poped
+; Input : none
+; Output: R0, number of line detected (D4-D7)
+; Modifies: R0 out, all used Register are pushed and poped
 detect_line
-	PUSH {R0,R1,LR}					; save current values of R0,R1,LR
-	POP {R0,R1,PC}					; restore previuos values
+	PUSH {R1,R2,R3,LR}			; save current values of R0,R1,LR
+	LDR R3,=GPIO_PORTD_0123			; R2 = &GPIO_PORTD_0123
+	LDR R0,[R3]						; R0 = [R2] (actual value of D0-D3)
+	MOV R1,#0xFF					; R1 = 0xFF
+detect_line_D4
+	LDR R2,=GPIO_PORTD_DATA+0x40	; R2 = &(GPIO_PORTD_DATA+0x040)(D4 mask)
+	STR R1,[R2]						; [R2] = R1 (set D4)
+	LDR R2,[R3]						; R2 = [R3] (GPIO_PORTD_0123)
+	CMP R0, R2						; R0 == R2 ? (no change on PORTD_0123 ?)
+	BEQ detect_line_D5				; if so, go to detect_line_D5
+	; acknowledge D4 line
+	MOV R0,#4
+	B	detect_line_done	
+detect_line_D5
+	LDR R2,=GPIO_PORTD_DATA+0x80	; R2 = &(GPIO_PORTD_DATA+0x080)(D5 mask)
+	STR R1,[R2]						; [R2] = R1 (set D5)
+	LDR R2,[R3]						; R2 = [R3] (GPIO_PORTD_0123)
+	CMP R0, R2						; R0 == R2 ? (no change on PORTD_0123 ?)
+	BEQ detect_line_D6				; if so, go to detect_line_D6
+	; acknowledge D5 line
+	MOV R0,#5
+	B	detect_line_done
+detect_line_D6
+	LDR R2,=GPIO_PORTD_DATA+0x100	; R2 = &(GPIO_PORTD_DATA+0x100)(D6 mask)
+	STR R1,[R2]						; [R2] = R1 (set D6)
+	LDR R2,[R3]						; R2 = [R3] (GPIO_PORTD_0123)
+	CMP R0, R2						; R0 == R2 ? (no change on PORTD_0123 ?)
+	BEQ detect_line_D7				; if so, go to detect_line_D7
+	; acknowledge D6 line
+	MOV R0,#6
+	B	detect_line_done
+detect_line_D7
+	LDR R2,=GPIO_PORTD_DATA+0x200	; R2 = &(GPIO_PORTD_DATA+0x200)(D7 mask)
+	STR R1,[R2]						; [R2] = R1 (set D7)
+	LDR R2,[R3]						; R2 = [R3] (GPIO_PORTD_0123)
+	CMP R0, R2						; R0 == R2 ? (no change on PORTD_0123 ?)
+	BEQ detect_line_done			; if so, go to detect_line_done
+	; acknowledge D5 line
+	MOV R0,#7
+	B	detect_line_done
+detect_line_done
+	; clear D4-D7
+	LDR R1,=GPIO_PORTD_4567			; R1 = GPIO_PORTD_4567 (pointer)
+	MOV R2,#0x00					; R2 = 0x00
+	STR R2,[R1]						; [R1] = R2 (clear D4-D7)	
+	POP {R1,R2,R3,PC}			; restore previuos values
 
 	ALIGN                           ; make sure the end of this section is aligned
     END                             ; end of file
